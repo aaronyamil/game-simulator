d8882c74de6d90f1597678ea256d192e2afc16f9
Aaron Luna <aaron.yamil@gmail.com>
2021-12-05 10:07:36 -0400

    calculate scored

--- src/container/ContainerTask2.js
+++ src/container/ContainerTask2.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Task2 } from "../components/Task2";
 
-export const ContainerTask2 = () => {
+export const ContainerTask2 = (props) => {
+  const [user, setUser] = useState({});
+  const [userScore, setUserScore] = useState(0);
+
+  const calculateScore = (userId) => {
+    props.data.forEach((element) => {
+      if (element.userId === userId) {
+        setUser(element);
+        console.log('score', (parseInt(element.level) * parseInt(element.coins)) + parseInt(element.time));
+        setUserScore((parseInt(element.level)*parseInt(element.coins)) + parseInt(element.time));
+
+      }})
+  }
   return (
     <div className="container-component">
-      <Task2 />
+      <Task2 calculateScore={calculateScore} user={user} userScore={userScore}/>
     </div>
   );
 };
